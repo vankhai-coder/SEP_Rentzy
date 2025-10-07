5befa0f2fff44a770c0ef3f912441160178f3440
ablue <tienvukk1@gmail.com>
2025-10-07 11:43:02 +0700

    fix

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,9 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 // import all models for sequelize to sync:
 import db from "./models/index.js";
@@ -47,7 +50,7 @@ app.get("/", (req, res) => {
 // sync database models
 (async () => {
   try {
-    await db.sequelize.sync({ force: true });
+    await db.sequelize.sync({ alter: true });
     console.log("✅ All models synced!");
   } catch (err) {
     console.error("❌ Error syncing models:", err);
