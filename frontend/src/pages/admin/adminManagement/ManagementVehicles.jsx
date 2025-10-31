a39f829c8611baaa7fe90d7da6e9395ce516d48d
thaibao02 <thaibaovu0212@gmail.com>
2025-10-31 22:53:18 +0700

    update sidebar admin

--- frontend/src/pages/admin/adminManagement/ManagementVehicles.jsx
+++ frontend/src/pages/admin/adminManagement/ManagementVehicles.jsx
@@ -212,7 +212,7 @@ const ManagementVehicles = () => {
                 <MdSearch className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400 w-5 h-5" />
                 <input
                   type="text"
-                  placeholder="Tìm kiếm theo biển số, model, địa điểm, tên chủ xe..."
+                  placeholder="Tìm kiếm theo tên , biển số ..."
                   value={searchQuery}
                   onChange={(e) => setSearchQuery(e.target.value)}
                   className="w-full pl-10 pr-4 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-transparent"
